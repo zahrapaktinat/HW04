31de1d769d9041bcb885bdea044cae9162a94e91
Zahra paktinat <78553642+zahrapaktinat@users.noreply.github.com>
2024-12-15 13:31:42 +0330

    fix validation

--- index.js
+++ index.js
@@ -1,34 +1,100 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.querySelector(".login-form");
-    if (loginForm) {
-      loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const username = document.querySelector("#username");
-        const password = document.querySelector("#password");
-        if (username.value.trim() === "" || password.value.trim() === "") {
-          alert("Both fields are required.");
-        } else {
-          loginForm.submit();
-        }
-      });
-    }
+const emailPattern = /^\S+@\S+\.\S+$/;
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.querySelector("#login-form");
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    const registerForm = document.querySelector(".register-form");
-    if (registerForm) {
-      registerForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const username = document.querySelector("#username");
-        const email = document.querySelector("#email");
-        const password = document.querySelector("#password");
-        const confirmPassword = document.querySelector("#confirm-password");
+      let valid = true;
 
-        if (username.value.trim() === "" || email.value.trim() === "" || password.value.trim() === "" || confirmPassword.value.trim() === "") {
-          alert("All fields are required.");
-        } else if (password.value !== confirmPassword.value) {
-          alert("Passwords do not match.");
-        } else {
-          registerForm.submit();
-        }
-      });
-    }
-  });
+      const emailInput = document.querySelector("#email");
+      emailInput.classList.remove("is-error");
+      const emailInputError = document.querySelector("#email-error");
+      emailInputError.textContent = "";
+
+      const password = document.querySelector("#password");
+      password.classList.remove("is-error");
+      const passwordError = document.querySelector("#password-error");
+      passwordError.textContent = "";
+
+      if (emailInput.value.trim() === "") {
+        emailInput.classList.add("is-error");
+        emailInputError.textContent = "This field is required.";
+        valid = false;
+      } else if (emailPattern.test(emailInput.value) === false) {
+        emailInput.classList.add("is-error");
+        emailInputError.textContent = "The email address is not valid.";
+        valid = false;
+      }
+
+      if (password.value.trim() === "") {
+        password.classList.add("is-error");
+        passwordError.textContent = "This field is required.";
+        valid = false;
+      }
+
+      if (valid) {
+        loginForm.submit();
+      }
+    });
+  }
+
+  const registerForm = document.querySelector("#register-form");
+  if (registerForm) {
+    registerForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      let valid = true;
+
+      const username = document.querySelector("#username");
+      const usernameError = document.querySelector("#username-error");
+      const email = document.querySelector("#email");
+      const emailError = document.querySelector("#email-error");
+      const password = document.querySelector("#password");
+      const confirmPassword = document.querySelector("#confirm-password");
+      const passwordError = document.querySelector("#password-error");
+
+      username.classList.remove("is-error");
+      email.classList.remove("is-error");
+      password.classList.remove("is-error");
+      confirmPassword.classList.remove("is-error");
+
+      usernameError.textContent = "";
+      emailError.textContent = "";
+      passwordError.textContent = "";
+
+      if (username.value.trim() === "") {
+        username.classList.add("is-error");
+        usernameError.textContent = "This field is required.";
+        valid = false;
+      }
+
+      if (email.value.trim() === "") {
+        email.classList.add("is-error");
+        emailError.textContent = "This field is required.";
+        valid = false;
+      } else if (emailPattern.test(email.value) === false) {
+        email.classList.add("is-error");
+        emailError.textContent = "The email address is not valid.";
+        valid = false;
+      }
+
+      if (password.value === "" || confirmPassword.value === "") {
+        password.classList.add("is-error");
+        confirmPassword.classList.add("is-error");
+
+        passwordError.textContent = "This field is required.";
+        valid = false;
+      } else if (password.value !== confirmPassword.value) {
+        password.classList.add("is-error");
+        confirmPassword.classList.add("is-error");
+        passwordError.textContent = "The passwords do not match.";
+
+        valid = false;
+      }
+
+      if (valid) {
+        registerForm.submit();
+      }
+    });
+  }
+});
